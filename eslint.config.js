b92a8d447eecda51e367b079a3cd77527739ea94
evrwaq <evrwaq@gmail.com>
2024-12-13 13:02:15 -0300

    chore: add linebreak-style rule

--- eslint.config.js
+++ eslint.config.js
@@ -42,6 +42,7 @@ export default tseslint.config(
         'error',
         { allowExpressions: false },
       ],
+      'linebreak-style': ['error', 'unix'],
       'prettier/prettier': ['error'],
     },
   }
